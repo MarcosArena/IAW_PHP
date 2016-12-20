f5970c70bc96a1424fbc6af4c2bc107fa17f2298
MarcosArena <caspcrew92@gmail.com>
2016-12-20 19:46:39 +0100

    Add files via upload

--- start.php
+++ start.php
@@ -15,7 +15,8 @@ if (!isset($_SESSION['dni'])) {
 
 }
 
-// Set the page title and include the HTML header:
+else {
+    // Set the page title and include the HTML header:
 $dni = $_SESSION['dni'];
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
@@ -24,26 +25,15 @@ $page_title = 'Logged In!';
 
 
 //We check the user ROLE
-if ($role == "big_boss") {
-    include ('includes/header_boss.html');    
-    
-}
 
-if ($role == "dep_boss") {
-    include ('includes/header_dep_boss.html');
-}
-
-if ($role =="employee") {
-    include('includes/header_emp.html');
-}
-
+include ('includes/header.html');    
 
 // Print a customized message:
-
-
-
 echo "<h1>Logged In!</h1>
 <p>Welcome $name, you are now logged in. Your DNI is: $dni and your role is $role</p>";
+
+}
+
 
 
 include ('includes/footer.html');
