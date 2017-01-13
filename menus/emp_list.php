7847a031c569426ba3820a27381437a625e15f4b
Sebastian Rodriguez <sebanacional@hotmail.com>
2017-01-13 16:02:16 +0100

    Update emp_list.php

--- menus/emp_list.php
+++ menus/emp_list.php
@@ -1,11 +1,7 @@
 <?php
-
-
 session_start(); // Access the existing session.
-
 // If no session variable exists, redirect the user:
 if (!isset($_SESSION['dni'])) {
-
 	// Need the functions:
 	require ('../includes/functions.php');
 	redirect_user('../login.php');	
@@ -14,11 +10,7 @@ if (!isset($_SESSION['dni'])) {
 		require ('../includes/functions.php');		
 		include ('../includes/header.php');
 		emplist();
-
 		// Check if the role is "Staff_Manager" and if it is, we show a Create User Button.	
-		if 
-
+		
 }
-
-
 ?>
