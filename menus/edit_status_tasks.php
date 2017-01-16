b8d61249afd59d97044009976c3fefe25bb131e4
Sebastian Rodriguez <sebanacional@hotmail.com>
2017-01-16 20:08:06 +0100

    Add files via upload
    archvos referentes al task.

---
+++ menus/edit_status_tasks.php
@@ -0,0 +1,90 @@
+<?php
+
+session_start(); // Access the existing session.
+// If no session variable exists, redirect the user:
+if (!isset($_SESSION['dni'])) {
+	// Need the functions:
+	require ('../includes/functions.php');
+	redirect_user('../login.php');	
+	
+} 
+else { 
+	$page_title = 'Edit the Status of the task';
+	echo '<h1>Edit Status Task</h1>';
+
+
+	// Check for a valid task ID, through GET Method:
+	//From see_tasks.php
+	if ( (isset($_GET['id'])) && (is_numeric($_GET['id'])) ) { 
+		$id = $_GET['id'];
+	} elseif ( (isset($_POST['id'])) && (is_numeric($_POST['id'])) ) { // Form submission.
+		$id = $_POST['id'];
+	} else { // No valid ID, kill the script.
+		echo '<p class="error">This page has been accessed in error.</p>';
+		exit();
+	}
+
+	require ('../includes/mysqli_connect.php');
+
+	// Check if the form has been submitted:
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+		//Update the record
+		if ($_POST['sure'] == 'Yes') { 
+			
+			// Make the query:
+			$q = "UPDATE tasks SET State='Pending', Time_Finish=NOW() WHERE id=$id LIMIT 1";		
+			$r = @mysqli_query ($dbc, $q);
+			if (mysqli_affected_rows($dbc) == 1) { // If it ran OK.
+
+				// Print a message and start the count redireccion
+				ob_start(); 
+				header("refresh: 3; url = see_tasks.php"); 
+				echo '<p>The task has been closing</p>';
+				echo 'Wait a moment...'; 
+
+				ob_end_flush();
+					
+
+			} else { // If the query did not run OK.
+				echo '<p class="error">The user could not be close task to a system error.</p>'; // Public message.
+				echo '<p>' . mysqli_error($dbc) . '<br />Query: ' . $q . '</p>'; // Debugging message.
+			}
+		
+		} else { // No confirmation of deletion.
+			echo '<p>The task has NOT been closing.</p>';	
+		}
+
+	} else { // Show the form.
+
+		// Retrieve the task information:
+		$q = "SELECT ID, Name, Description, Time_Start, Time_Finish, State, Employee, Department FROM tasks WHERE id=$id";		
+		$r = @mysqli_query ($dbc, $q);
+
+		if (mysqli_num_rows($r) == 1) { // Valid task ID, show the form.
+
+			// Get the task information:
+			$row = mysqli_fetch_array ($r, MYSQLI_NUM);
+			
+			// Display the record being closed:
+			echo "<h3>Name: $row[1]</h3>
+			
+			Are you sure you want to close this task?";
+			
+			// Create the form:
+			echo '<form action="edit_status_tasks.php" method="post">
+		<input type="radio" name="sure" value="Yes" /> Yes 
+		<input type="radio" name="sure" value="No" checked="checked" /> No
+		<input type="submit" name="submit" value="Submit" />
+		<input type="hidden" name="id" value="' . $id . '" />
+		</form>';
+		
+		} else { // Not a valid task ID.
+			echo '<p class="error">This page has been accessed in error.</p>';
+		}
+
+	} // End of the main submission conditional.
+
+	mysqli_close($dbc);
+}	
+?>
--- menus/see_tasks.php
+++ menus/see_tasks.php
@@ -1,71 +1,193 @@
--- EMPLEADOS DEL DEPARTAMENTO.
-
-
-
--- ENSEÑA TODAS LAS TAREAS DEL DEPARTAMENTO
-
-Los usuarios pueden ver las tarea y "precerrarlas" UPTADATE de STATE => "Finished" 
-
 <?php
-//incluir header
-include '../includes/header.php';
-//Start web
-$page_title = 'View the department tasks';
-echo '<h1>Department tasks</h1>';
-require ('../includes/mysqli_connect.php');
-
-// Define the select query:
-$q = "SELECT ID, Name, Description, Time_Start, Time_Finish, State, Employee, Department FROM tasks ORDER BY ID";		
-$r = @mysqli_query ($dbc, $q);
-
-// Count the number of returned rows:
-$num = mysqli_num_rows($r);
-
-// If it ran OK, display the records.
-if ($num > 0) { 
-    // Print how many tasks there are:
-	echo "<p>There are currently $num tasks.</p>\n";
-
-    // Table header:
-	echo '<link rel="stylesheet" href="../style/users_data.css">
+session_start(); // Access the existing session.
+// If no session variable exists, redirect the user:
+if (!isset($_SESSION['dni'])) {
+	// Need the functions:
+	require ('../includes/functions.php');
+	redirect_user('../login.php');	
 	
-	<table id="hor-minimalist-a" align="center">
-	<tr>
-
-		<td align="center"><b>ID</b></td>
-		<td align="center"><b> Name</b></td>
-		<td align="center"><b> Description</b></td>
-		<td align="center"><b> Time_Start</b></td>
-		<td align="center"><b> Time_Finish</b></td>
-		<td align="center"><b> State</b></td>	
-        <td align="center"><b> Employee</b></td>	
-        <td align="center"><b> Department</b></td>		
-	</tr>';
-
-    // Fetch and print all the records:
-	while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
-		echo '<tr>
-
-			<td align="left">' . $row[0] . '</td>
-			<td align="left">' . $row[1] . '</td>
-			<td align="left">' . $row[2] . '</td>
-			<td align="left">' . $row[3] . '</td>
-			<td align="left">' . $row[4] . '</td>
-			<td align="left">' . $row[5] . '</td>
-            <td align="left">' . $row[6] . '</td>
-            <td align="left">' . $row[7] . '</td>
-		</tr>';
-    }
-    echo '</table>';
-	mysqli_free_result ($r);
-
-}
+} 
 else { 
-    // If no records were returned.
-	echo '<p class="error">There are currently no registered tasks.</p>';
+	//incluir header
+	include '../includes/header.php';
+
+	//Start web
+	$page_title = 'View the department tasks';
+	echo '<h1>Department tasks</h1>';
+	require ('../includes/mysqli_connect.php');
+
+	//insert tasks
+		if ($_SESSION['role']="dep_boss") {?>
+			<li><a href="#"  onclick=window.open('./inserttask.php','ventana','width=640,height=600');
+			>Insert tasks</a></li>
+			<li><a href="#" onclic="header('location:pagina.php')">Refresh</a></li>
+		<?php
+		}
+	// Define the select query:
+	$q = "SELECT t.ID, t.Name, t.Description, t.Time_Start, t.Time_Finish, t.State, e.Name, d.Name FROM tasks AS t 
+		  INNER JOIN department as d  ON t.Department = d.Code
+		  INNER JOIN employees as e on t.Employee = e.DNI 
+		  ORDER BY t.ID";		
+	$r = @mysqli_query ($dbc, $q);
+
+	// Count the number of returned rows:
+	$num = mysqli_num_rows($r);
+
+	// If it ran OK, display the records.
+	if ($num > 0) { 
+
+		if ($_SESSION['role']="dep_boss"){
+			//The Dep Boss is authorized to delete and close definitly the tasks
+						echo "<p>There are currently $num tasks.</p>\n";
+
+			// Table header:
+			echo '<link rel="stylesheet" href="../style/users_data.css">
+			
+			<table id="hor-minimalist-a" align="center">
+			<tr>
+
+				<td align="center"><b>ID</b></td>
+				<td align="center"><b> Name</b></td>
+				<td align="center"><b> Description</b></td>
+				<td align="center"><b> Time_Start</b></td>
+				<td align="center"><b> Time_Finish</b></td>
+				<td align="center"><b> State</b></td>	
+				<td align="center"><b> Employee</b></td>	
+				<td align="center"><b> Department</b></td>
+				<td align="center"><b>Close Task</b></td>
+				<td align="center"><b> Delete Task</b></td>			
+			</tr>';
+
+			// Fetch and print all the records:
+			while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
+				if('Pending' == $row[5]){
+					echo '<tr>
+
+					<td align="left">' . $row[0] . '</td>
+					<td align="left">' . $row[1] . '</td>
+					<td align="left">' . $row[2] . '</td>
+					<td align="left">' . $row[3] . '</td>
+					<td align="left">' . $row[4] . '</td>
+					<td align="left">' . $row[5] . '</td>
+					<td align="left">' . $row[6] . '</td>
+					<td align="left">' . $row[7] . '</td>
+					<td align="left"><a href="confirm_close_task.php?id=' . $row[0] . '">Confirm Close</a></td>
+					<td align="left"><a href="delete_task.php?id=' . $row[0] . '">Delete Task</a></td>
+				</tr>';
+				}
+				elseif ('Finished' == $row[5]){
+					echo '<tr>
+
+					<td align="left">' . $row[0] . '</td>
+					<td align="left">' . $row[1] . '</td>
+					<td align="left">' . $row[2] . '</td>
+					<td align="left">' . $row[3] . '</td>
+					<td align="left">' . $row[4] . '</td>
+					<td align="left">' . $row[5] . '</td>
+					<td align="left">' . $row[6] . '</td>
+					<td align="left">' . $row[7] . '</td>
+					<td align="left"></td>
+					<td align="left"><a href="delete_task.php?id=' . $row[0] . '">Delete Task</a></td>
+				</tr>';
+				}
+				else{
+					echo '<tr>
+
+					<td align="left">' . $row[0] . '</td>
+					<td align="left">' . $row[1] . '</td>
+					<td align="left">' . $row[2] . '</td>
+					<td align="left">' . $row[3] . '</td>
+					<td align="left">' . $row[4] . '</td>
+					<td align="left">' . $row[5] . '</td>
+					<td align="left">' . $row[6] . '</td>
+					<td align="left">' . $row[7] . '</td>
+					<td align="left"><a href="edit_status_tasks.php?id=' . $row[0] . '">Close Task</a></td>
+					<td align="left"><a href="delete_task.php?id=' . $row[0] . '">Delete Task</a></td>
+				</tr>';
+				}
+
+			}
+		}
+		else{
+				//all the users
+				// Print how many tasks there are:
+			echo "<p>There are currently $num tasks.</p>\n";
+
+			// Table header:
+			echo '<link rel="stylesheet" href="../style/users_data.css">
+			
+			<table id="hor-minimalist-a" align="center">
+			<tr>
+
+				<td align="center"><b>ID</b></td>
+				<td align="center"><b> Name</b></td>
+				<td align="center"><b> Description</b></td>
+				<td align="center"><b> Time_Start</b></td>
+				<td align="center"><b> Time_Finish</b></td>
+				<td align="center"><b> State</b></td>	
+				<td align="center"><b> Employee</b></td>	
+				<td align="center"><b> Department</b></td>
+				<td align="center"><b>Close Task</b></td>		
+			</tr>';
+
+			// Fetch and print all the records:
+			while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
+				if('Pending' == $row[5]){
+					echo '<tr>
+
+					<td align="left">' . $row[0] . '</td>
+					<td align="left">' . $row[1] . '</td>
+					<td align="left">' . $row[2] . '</td>
+					<td align="left">' . $row[3] . '</td>
+					<td align="left">' . $row[4] . '</td>
+					<td align="left">' . $row[5] . '</td>
+					<td align="left">' . $row[6] . '</td>
+					<td align="left">' . $row[7] . '</td>
+				</tr>';
+				}
+				elseif ('Finished' == $row[5]){
+					echo '<tr>
+
+					<td align="left">' . $row[0] . '</td>
+					<td align="left">' . $row[1] . '</td>
+					<td align="left">' . $row[2] . '</td>
+					<td align="left">' . $row[3] . '</td>
+					<td align="left">' . $row[4] . '</td>
+					<td align="left">' . $row[5] . '</td>
+					<td align="left">' . $row[6] . '</td>
+					<td align="left">' . $row[7] . '</td>
+				</tr>';
+				}
+				else{
+					echo '<tr>
+
+					<td align="left">' . $row[0] . '</td>
+					<td align="left">' . $row[1] . '</td>
+					<td align="left">' . $row[2] . '</td>
+					<td align="left">' . $row[3] . '</td>
+					<td align="left">' . $row[4] . '</td>
+					<td align="left">' . $row[5] . '</td>
+					<td align="left">' . $row[6] . '</td>
+					<td align="left">' . $row[7] . '</td>
+					<td align="left"><a href="edit_status_tasks.php?id=' . $row[0] . '">Close Task</a></td>
+				</tr>';
+				}
+
+			}
+			echo '</table>';
+			mysqli_free_result ($r);
+
+		}
+
+		}
+		
+else { 
+		// If no records were returned.
+		echo '<p class="error">There are currently no registered tasks.</p>';
+	}
+	mysqli_close($dbc);
 }
 
-mysqli_close($dbc);
 
 
 ?>
