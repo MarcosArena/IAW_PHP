8cb18bdc1b55f6240cc0db7ba84c5ca7e4fef2fa
MarcosArena <caspcrew92@gmail.com>
2017-01-17 20:39:40 +0100

    Add files via upload

--- includes/mysqli_connect.php
+++ includes/mysqli_connect.php
@@ -1,8 +1,8 @@
 <?php
-DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PASSWORD', '');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'iaw');
+DEFINE ('DB_USER', 'u773614403_jms');
+DEFINE ('DB_PASSWORD', 'PHPJMS');
+DEFINE ('DB_HOST', 'mysql.hostinger.es');
+DEFINE ('DB_NAME', 'u773614403_iaw');
 
 // Make the connection:
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
